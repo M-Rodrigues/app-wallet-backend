4f3f153e27680c3c4401a1be798d58203b7a56a3
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2019-08-27 23:43:53 -0300

    changing db

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,17 +4,17 @@ import mongoose from "mongoose";
 const connector = {
   mongoose,
   connect() {    
-    mongoose.connect('mongodb://localhost:27017/app-wallet', {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      auth: {
-        user: "root",
-        password: "example"
-      },
-      authSource: 'admin'
-    // mongoose.connect('mongodb+srv://app-wallet-db:server_app_wallet@app-wallet-db-xnrz8.mongodb.net/test', {
+    // mongoose.connect('mongodb://localhost:27017/app-wallet', {
     //   useNewUrlParser: true,
     //   useCreateIndex: true,
+    //   auth: {
+    //     user: "root",
+    //     password: "example"
+    //   },
+    //   authSource: 'admin'
+    mongoose.connect('mongodb+srv://app-wallet-db:server_app_wallet@app-wallet-db-xnrz8.mongodb.net/test', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
     }).then(() => {
       // console.log('successfully connected to the database');
     }, err => {
