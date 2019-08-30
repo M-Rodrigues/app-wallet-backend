bba04fb5cc1610f9bf23d8a9bd2f60c18a606263
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2019-08-29 22:15:42 -0300

    mongo

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,25 +1,14 @@
 import mongoose from "mongoose";
 
 
-const connString = process.env.MONGO_CONN_STRING || "mongodb://root:example@localhost:27017/app-wallet&auth=admin"; 
+const connString = process.env.MONGO_CONN_STRING || "mongodb://root:example@localhost:27017/app-wallet"; 
 const connector = {
   mongoose,
   connect() {    
     mongoose.connect(connString, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      // authSource: 'admin',
-    // mongoose.connect('mongodb+srv://app-wallet-db:server_app_wallet@app-wallet-db-xnrz8.mongodb.net/test', {
-    //   useNewUrlParser: true,
-    //   useCreateIndex: true,
-    //   auth: {
-    //     user: "root",
-    //     password: "example"
-    //   },
-    //   authSource: 'admin'
-    // mongoose.connect('mongodb+srv://app-wallet-db:server_app_wallet@app-wallet-db-xnrz8.mongodb.net/test', {
-    //   useNewUrlParser: true,
-    //   useCreateIndex: true,
+      authSource: 'admin',
     }).then(() => {
       // console.log('successfully connected to the database');
     }, err => {
